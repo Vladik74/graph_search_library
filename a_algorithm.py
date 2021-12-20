92ee149f04bb617c4ae4747e5ced1471453da2b6
DILYARA <fatykhova.dilyara@mail.ru>
2021-12-21 02:15:36 +0500

    a(star)_algorithm(not_finished)

--- a_algorithm.py
+++ a_algorithm.py
@@ -1,20 +1,44 @@
-frontier = PriorityQueue()
-frontier.put(start, 0)
-came_from = {}
-cost_so_far = {}
-came_from[start] = None
-cost_so_far[start] = 0
+from heapq import *
 
-while not frontier.empty():
-    current = frontier.get()
+graph = {'0': [(2, '2'), (3, '4')],
+         '2': [(2, '0'), (2, '3')],
+         '3': [(2, '2'), (2, '1')],
+         '4': [(3, '0'), (4, '1')],
+         '1': [(4, '4'), (2, '3')]}
 
-    if current == goal:
-        break
 
-    for next in graph.neighbors(current):
-        new_cost = cost_so_far[current] + graph.cost(current, next)
-        if next not in cost_so_far or new_cost < cost_so_far[next]:
-            cost_so_far[next] = new_cost
-            priority = new_cost + heuristic(goal, next)
-            frontier.put(next, priority)
-            came_from[next] = current
+def heuristic(a, b):
+    return abs(int(a[0]) - int(b[0])) + abs(int(a[1]) - int(b[1]))
+
+
+def a_algorithm(start, goal, graph):
+    queue = []
+    heappush(queue, (0, start))
+    cost_visited = {start: 0}
+    visited = {start: None}
+
+    while queue:
+        cur_cost, cur_node = heappop(queue)
+        if cur_node == goal:
+            break
+
+        next_nodes = graph[cur_node]
+        for next_node in next_nodes:
+            neigh_cost, neigh_node = next_node
+            new_cost = cost_visited[cur_node] + neigh_cost
+
+            if neigh_node not in cost_visited or new_cost < cost_visited[neigh_node]:
+                priority = new_cost + heuristic(neigh_node, goal)
+                heappush(queue, (priority, neigh_node))
+                cost_visited[neigh_node] = new_cost
+                visited[neigh_node] = cur_node
+    return visited
+
+
+start = '0'
+goal = '1'
+cur_node = goal
+visited = a_algorithm(start, goal, graph)
+while cur_node != start:
+    cur_node = visited[cur_node]
+    print(cur_node)
